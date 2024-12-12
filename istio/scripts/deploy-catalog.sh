ed34da4583a167c9ab5e8fdcc6bdb76fa0aa60e1
rh-bjeon <108232840+rh-bjeon@users.noreply.github.com>
2024-12-13 00:24:37 +0900

    Update deploy-catalog.sh

--- istio/scripts/deploy-catalog.sh
+++ istio/scripts/deploy-catalog.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-USERXX=$1
-DELAY=$2
-
-if [ -z "$USERXX" -o "$USERXX" = "userXX" ]
-  then
-    echo "Usage: Input your username like deploy-catalog.sh user1"
-    exit;
-fi
 
 echo Your username is $USERXX
 echo Deploy Catalog service........
 
-oc project $USERXX-catalog || oc new-project $USERXX-catalog
+oc project catalog || oc new-project catalog
 oc delete dc,deployment,bc,build,svc,route,pod,is --all
 
 echo "Waiting 30 seconds to finialize deletion of resources..."
 sleep 30
-
-sed -i "s/userXX/${USERXX}/g" $PROJECT_SOURCE/catalog/src/main/resources/application.properties
 
 oc new-app --as-deployment-config -e POSTGRESQL_USER=catalog \
              -e POSTGRESQL_PASSWORD=mysecretpassword \
@@ -26,7 +16,7 @@ oc new-app --as-deployment-config -e POSTGRESQL_USER=catalog \
              openshift/postgresql:10-el8 \
              --name=catalog-database
 
-mvn clean install -Ddekorate.deploy=true -DskipTests -f $PROJECT_SOURCE/catalog
+mvn clean install -Ddekorate.deploy=true -DskipTests -f catalog
 
 oc label dc/catalog-database app.openshift.io/runtime=postgresql --overwrite && \
 oc label dc/catalog-springboot app.openshift.io/runtime=spring-boot --overwrite && \
